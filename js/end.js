aeff30deb0f7a82d23285397c9ed72bafee404f6
MachinEmmus <emonsalvep38@gmail.com>
2020-12-07 11:49:51 -0500

    Add HihgScore Table and add a fetch to search the questions in json file

--- js/end.js
+++ js/end.js
@@ -2,6 +2,8 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
@@ -10,4 +12,15 @@ username.addEventListener('keyup', () => {
 
 saveHighScore = (e) => {
     e.preventDefault();
+
+    const Score = {
+        score: mostRecentScore,
+        name: username.value,
+    };
+    highScores.push(Score);
+    highScores.sort((a, b) => b.Score - a.Score);
+    highScores.splice(5);
+    localStorage.setItem('highScores', JSON.stringify(highScores));
+    console.log(highScores);
+    //window.location.assign("../html/index.html");
 };
